0d1bd79cae9d4c76e531e64ab11c36c9da637e2d
christoforoskon <xristoforos.knks@yahoo.gr>
2022-11-13 18:50:05 +0200

    Navigation functinality added

--- src/App.js
+++ src/App.js
@@ -2,54 +2,57 @@ import React from "react";
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-import Rank from './components/Rank/Rank';
-import FaceRecognition from './components/FaceRecognition/FaceRecognition';
-import ParticlesBg from 'particles-bg';
-import Clarifai from 'clarifai';
-import axios, * as others from 'axios';
-
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
+import ParticlesBg from "particles-bg";
+import Clarifai from "clarifai";
+import axios, * as others from "axios";
+import SignIn from "./components/SignIn/SignIn";
+import Register from "./components/Register/Register";
 
 const app = new Clarifai.App({
-  apiKey: '32efda695ffd48c299dc071f0e387f50'
+  apiKey: "32efda695ffd48c299dc071f0e387f50",
 });
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      input: '',
-      imageUrl: ''
-    }
+      input: "",
+      imageUrl: "",
+      route: 'signin',
+      isSignedIn: false
+    };
   }
 
   calculateFaceLocation = (data) => {
-    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById('inputimage');
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputimage");
     const width = Number(image.width);
     const height = Number(image.height);
     // console.log(width, height);
     return {
       leftCol: clarifaiFace.left_col * width,
       topRow: clarifaiFace.top_row * height,
-      rightCol: width - (clarifaiFace.right_col * width),
-      bottomRow: height - (clarifaiFace.bottom_row * height),
-    }
-  }
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
+  };
 
   displayFaceBox = (box) => {
     console.log(box);
     this.setState({ box: box });
-  }
+  };
 
   onInputChange = (event) => {
     this.setState({ input: event.target.value });
-  }
+  };
 
   // .predict('53e1df302c079b3db8a0a36033ed2d15', this.state.input)
   // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
   onButtonSubmit = () => {
-
     this.setState({ imageUrl: this.state.input });
     app.models
       .predict(
@@ -63,23 +66,46 @@ class App extends React.Component {
         // to:
         // .predict('53e1df302c079b3db8a0a36033ed2d15', this.state.input)
         Clarifai.FACE_DETECT_MODEL,
-        this.state.input)
-      .then(response => {
-        console.log('hi', response)
-        this.displayFaceBox(this.calculateFaceLocation(response))
+        this.state.input
+      )
+      .then((response) => {
+        console.log("hi", response);
+        this.displayFaceBox(this.calculateFaceLocation(response));
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
+  };
+
+  onRouteChange = (route) => {
+    if (route === "signout") {
+      this.setState({ isSignedIn: false });
+    } else if (route === "home") {
+      this.setState({ isSignedIn: true })
+    }
+    this.setState({ route: route });
   }
 
-
   render() {
+    const { isSignedIn, imageUrl, route } = this.state;
     return (
       <div className="App">
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
-        <FaceRecognition imageUrl={this.state.imageUrl} />
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+        {route === "home"
+          ? <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+            />
+            <FaceRecognition imageUrl={imageUrl} />
+          </div>
+          : (
+            this.state.route === "signin"
+              ? < SignIn onRouteChange={this.onRouteChange} />
+              : < Register onRouteChange={this.onRouteChange} />
+          )
+
+        }
       </div>
     );
   }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
-const Navigation = () => {
-  return (
-    <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
-      <p className='link dim dark-gray underline f3 pointer ma4'>Sing Out</p>
-    </nav >
-  );
+const Navigation = ({ onRouteChange, isSignedIn }) => {
+
+  if (isSignedIn) {
+    return (
+      <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <p className='link dim dark-gray underline f3 pointer ma4' onClick={() => onRouteChange('signout')}>Sing Out</p>
+      </nav >
+    );
+  } else {
+    return (
+      <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <p className='link dim dark-gray underline f3 pointer ma4' onClick={() => onRouteChange('signin')}>Sing In</p>
+        <p className='link dim dark-gray underline f3 pointer ma4' onClick={() => onRouteChange('register')}>Register</p>
+      </nav >
+    );
+  }
 }
 
 export default Navigation;
