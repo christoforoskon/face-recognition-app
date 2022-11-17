96014edb08b6cff5508f26051ae56b26b634e91e
christoforoskon <xristoforos.knks@yahoo.gr>
2022-11-18 01:56:13 +0200

    init fix

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import ParticlesBg from "particles-bg";
 import Clarifai from "clarifai";
 import SignIn from "./components/SignIn/SignIn";
 import Register from "./components/Register/Register";
-const axios = require("axios").default;
 
 const app = new Clarifai.App({
   apiKey: "32efda695ffd48c299dc071f0e387f50",
@@ -56,63 +55,52 @@ class App extends React.Component {
   // }
 
   calculateFaceLocation = (data) => {
-    const clarifaiFace =
-      data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById("inputimage");
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
     const width = Number(image.width);
     const height = Number(image.height);
     return {
       leftCol: clarifaiFace.left_col * width,
       topRow: clarifaiFace.top_row * height,
-      rightCol: width - clarifaiFace.right_col * width,
-      bottomRow: height - clarifaiFace.bottom_row * height,
-    };
-  };
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height),
+    }
+  }
 
 
   displayFaceBox = (box) => {
-    this.setState({ box: box });
-  };
+    this.setState({ box: box })
+  }
 
   onInputChange = (event) => {
     this.setState({ input: event.target.value });
-  };
+  }
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
     app.models
       .predict(
-        // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
-        // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
-        // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
-        // If that isn't working, then that means you will have to wait until their servers are back up. Another solution
-        // is to use a different version of their model that works like the ones found here: https://github.com/Clarifai/clarifai-javascript/blob/master/src/index.js
-        // so you would change from:
-        // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-        // to:
-        // .predict('53e1df302c079b3db8a0a36033ed2d15', this.state.input)
         Clarifai.FACE_DETECT_MODEL,
-        this.state.input
-      )
-      .then((response) => {
-        console.log("hi", response);
+        this.state.input)
+      .then(response => {
         if (response) {
-          fetch("http://localhost:3000/image", {
-            method: "put",
-            headers: { "Content-Type": "application/json" },
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
-              id: this.state.user.id,
-            }),
+              id: this.state.user.id
+            })
           })
-            .then((response) => response.json())
-            .then((count) => {
-              this.setState(Object.assign(this.state.user, { entries: count }));
-            });
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, { entries: count }))
+            })
+
         }
-        this.displayFaceBox(this.calculateFaceLocation(response));
+        this.displayFaceBox(this.calculateFaceLocation(response))
       })
-      .catch((err) => console.log(err));
-  };
+      .catch(err => console.log(err));
+  }
 
   onRouteChange = (route) => {
     if (route === 'signout') {
@@ -128,21 +116,26 @@ class App extends React.Component {
     const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
-        <ParticlesBg type="fountain" bg={true} />
+
         <Navigation
           isSignedIn={isSignedIn}
           onRouteChange={this.onRouteChange}
         />
+        {this.state.route === 'home'
+          ? <div>
+            <Rank name={this.state.user.name} entries={this.state.user.entries} />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit} />
+            <FaceRecognition box={box} imageUrl={imageUrl} />
+          </div>
+          : (this.state.route === 'signin'
+            ? <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+            : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+          )
+        }
 
-        <div>
-          <Logo />
-          <Rank name={this.state.user.name} entries={this.state.user.entries} />
-          <ImageLinkForm
-            onInputChange={this.onInputChange}
-            onButtonSubmit={this.onButtonSubmit}
-          />
-          <FaceRecognition box={box} imageUrl={imageUrl} />
-        </div>
+
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
 // import registerServiceWorker from './registerServiceWorker';
-import 'tachyons';
+import "tachyons";
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 
-root.render(<App />)
+root.render(<App />);
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
-
